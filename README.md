f562c49a9eae2226137542d4e7c568ac85dae9af
zuokun300 <3270477719@qq.com>
2020-01-03 10:20:48 +0800

    mybatis-insert

--- README.md
+++ README.md
@@ -6,4 +6,23 @@
 [github 社区项目](https://github.com/zuokun300/community)
 [BootStrap组件](https://v3.bootcss.com/components/)
 [GitHubAPP登陆API  OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+
 ##工具
+git
+visual-paradigm
+
+##脚本
+`sql
+create table USER
+(
+  ID           INTEGER default NEXT VALUE FOR "PUBLIC"."SYSTEM_SEQUENCE_2D351212_B8BD_4C79_9D9E_00EC3B30F511"
+    primary key,
+  ACCOUNT_ID   VARCHAR(100),
+  NAME         VARCHAR(50),
+  TOKEN        CHAR(36),
+  GMT_CREATE   BIGINT,
+  GMT_MODIFIED BIGINT
+);
+
+`
+
--- src/main/java/itzk/study/community/community/controller/AuthorizeController.java
+++ src/main/java/itzk/study/community/community/controller/AuthorizeController.java
@@ -3,12 +3,17 @@ package itzk.study.community.community.controller;
 
 import itzk.study.community.community.dto.AccessTokenDTO;
 import itzk.study.community.community.dto.GithubUser;
+import itzk.study.community.community.mapper.UserMapper;
+import itzk.study.community.community.model.User;
 import itzk.study.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,9 +29,15 @@ public class AuthorizeController {
 
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request
+                           ){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
@@ -34,8 +45,21 @@ public class AuthorizeController {
         accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登陆成功，写session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else {
+            //登陆失败，重新登陆
+            return "redirect:/";
+        }
     }
 }
